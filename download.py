7da7d778aa2b7b686e88f1e361795abc8a036ca7
fotis <fotisp87@gmail.com>
2014-12-12 17:20:55 +0100

    new download.py

--- download.py
+++ download.py
@@ -8,73 +8,130 @@ import subprocess
 import json
 import couchdb
 import protobuf_to_dict
-
+import gearman
+import redis
+import os
+import shutil
 from pprint import pprint
-
+from redis import WatchError
 from config import *
 from googleplay import GooglePlayAPI
 from helpers import sizeof_fmt
 
-if (len(sys.argv) < 2):
-    print "Usage: %s packagename [filename]"
-    print "Download an app."
-    print "If filename is not present, will write to packagename.apk."
-    sys.exit(0)
-# Arguments only for testing, normally it will take package name from search results
-packagename = sys.argv[1]
 
-if (len(sys.argv) == 3):
-    filename = sys.argv[2]
-else:
-    filename = packagename + ".apk"
-
-# Connect
+nb_res = 3
+offset = None
+# Authenticating to Google Play
 api = GooglePlayAPI(ANDROID_ID)
 api.login(GOOGLE_LOGIN, GOOGLE_PASSWORD, AUTH_TOKEN)
 
-# Get the version code and the offer type from the app details
-m = api.details(packagename) # This API call returns all the medata we need in google's protobuf format
-prot = m.docV2 # protobuf
-#f = open("./results/" + filename + ".details", "wb")
-#print >> f, prot 
+# Establishing redis & gearman connections
+gm_worker = gearman.GearmanWorker(['127.0.0.1:4730'])
+redis_conn = redis.Redis(host="127.0.0.1", port=6379, db=0)
+print "Registered and waiting for work"
+def task_listener_reverse(gearman_worker, gearman_job):
+	
+	search_term = gearman_job.data
+	print "Will search for: " + search_term
+	try:
+		message = api.search(search_term, nb_res, offset)
+	except:
+		print "Error: something went wrong. Maybe the nb_res you specified was too big?"
+		sys.exit(1)
+	doc = message.doc[0]
+	for c in doc.child:
+		print c.docid
+		print c.title
 
-details = protobuf_to_dict.protobuf_to_dict(prot.details) # This serializes google's protobuf to JSON
-rating = protobuf_to_dict.protobuf_to_dict(prot.aggregateRating)
-description = prot.descriptionHtml
-metadata = { 'docid': prot.docid, 'creator': prot.creator, 'title': prot.title, 'details': details['appDetails'], 'rating': rating }
-print metadata
-#sa = doc.annotations.sectionRelated.listUrl
-#print sa
-#resp = api.browse(sa)
-#print resp
+		packagename = c.docid
+		try:
+			m = api.details(packagename) # This API call returns all the medata we need in google's protobuf format
+		except:
+			print "Error getting details from API"
+		prot = m.docV2 # protobuf
 
-#Open couchdb connection
-couch = couchdb.Server()
-db = couch['decompiled']
-#db.save(metadata)
+		# Check if apk has already been processed
+		pipe = redis_conn
+		try:
+			#pipe.watch(gearman_job.data)
+			rv = redis_conn.get(packagename)
+			if rv == None:
+				print "Inserting at redis: " + packagename
+				#pipe.multi()
+				pipe.set(packagename, prot.details.appDetails.versionCode)
+				#pipe.execute()
+			elif rv < prot.details.appDetails.versionCode:
+				print "Updating at redis: " + packagename
+				#pipe.multi()
+				pipe.set(packagename, prot.details.appDetails.versionCode)
+				#pipe.execute()
+			else:
+				print "File already done.."
+				continue
+		except WatchError:
+			print "error:" + os.getpid() + "encountered collision and did not change"
+			#pipe.reset()
+			continue
+		#finally:
+			#pipe.reset()
+		filename = packagename + ".apk"
+		details = protobuf_to_dict.protobuf_to_dict(prot.details) # This serializes google's protobuf to JSON
+		rating = protobuf_to_dict.protobuf_to_dict(prot.aggregateRating)
+		description = prot.descriptionHtml
+		metadata = { 'docid': prot.docid, 'creator': prot.creator, 'title': prot.title, 'details': details['appDetails'], 'rating': rating }
 
-#print m
-print "================================"
-#print doc
-vc = prot.details.appDetails.versionCode
-ot = prot.offer[0].offerType
+		#Open couchdb connection
+		try:
+			couch = couchdb.Server()
+		except:
+			print "Error connecting to db"
+		db = couch['decompiled']
+		#db.save(metadata)
 
-# Download (in ./apks/ folder for DroidBroker to find them)
-print "Downloading %s..." % sizeof_fmt(prot.details.appDetails.installationSize),
-data = api.download(packagename, vc, ot) # Download :D
-open("./apks/" + filename, "wb").write(data)
-print "Done"
-print "Running DroidBroker Tool"
+		#print m
+		print "================================"
+		#print doc
+		vc = prot.details.appDetails.versionCode
+		ot = prot.offer[0].offerType
 
-#Run Droid Broker 
-args = ['apktool','d',  packagename + '.apk','-f']
-args1 = ['java','-jar', 'DroidBroker.jar', '-P']
-subprocess.call(args1)
+		# Download (in ./apks/ folder for DroidBroker to find them)
+		print "Downloading %s..." % sizeof_fmt(prot.details.appDetails.installationSize),
+		data = api.download(packagename, vc, ot) # Download :D
+		f = open("./apks/" + filename, "wb")
+		f.write(data)
+		print "Done"
+		f.close()
+		print "Running DroidBroker Tool"
+		'''
+		#Run Droid Broker 
+		args = ['java','-jar', 'DroidBroker.jar', '-P', '-g']
+		
+		subprocess.call(args)
 
-json_filename=("./results/" + packagename + '.json')
+		json_filename=("./results/" + packagename + '.json')
 
-broker_results = open("./results/" + packagename + '.json')
-broker_json = json.load(broker_results)
-#pprint(broker_json)
-db_doc = { 'metadata': metadata, 'decompiled': broker_json }
-db.save(db_doc)
+		broker_results = open("./results/" + packagename + '.json')
+		broker_json = json.load(broker_results)
+		#pprint(broker_json)
+		broker_results.close()
+		'''
+		broker_json = { 'test': 'test'}
+		db_doc = { 'metadata': metadata, 'decompiled': broker_json }
+		db.save(db_doc)
+
+		print "Removing files..."
+		#DELETE FILES
+		try:
+			shutil.rmtree("./results" + packagename)
+			print "Removing: " + "./apks/" + filename
+			os.remove("./apks/" + filename)
+		except:
+			print "Failed to remove files"
+	return gearman_job.data + str(os.getpid())
+	
+# gm_worker.set_client_id is optional
+#gm_worker.set_client_id('python-worker')
+gm_worker.register_task('reverse', task_listener_reverse)
+print "I am goint to start working now"
+# Enter our work loop and call gm_worker.after_poll() after each time we timeout/see socket activity
+gm_worker.work()
